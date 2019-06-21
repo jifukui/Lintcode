bbdfc59ac70bad37a810e3aa7c8300442a8e128e
jifukui <805013833@qq.com>
2019-06-21 13:28:00 +0800

    Lint 6

--- 6. 合并排序数组 II/main.c
+++ 6. 合并排序数组 II/main.c
@@ -4,72 +4,72 @@ using namespace std;
 vector<int> mergeSortedArray(vector<int> &A, vector<int> &B) ;
 int main()
 {
-    int i,len;
-    vector <int> a(1);
-    vector <int> b(1);
-    vector <int> c;
-    c=mergeSortedArray(a,b);
-    len=c.size();
-    for(i=0;i<len;i++)
-    {
-        cout<<i<<"is"<<c[i]<<endl;
-    }
+    int i, len;
+	vector <int> a(4);
+	vector <int> b(4);
+	vector <int> c;
+	a[0] = 1;
+	a[1] = 2;
+	a[2] = 3;
+	a[3] = 4;
+	b[0] = 2;
+	b[1] = 4;
+	b[2] = 5;
+	b[3] = 6;
+	c = mergeSortedArray(a, b);
+	len = c.size();
+	for (i = 0; i<len; i++)
+	{
+		cout << i << "is" << c[i] << endl;
+	}
+	return 0;
 }
 vector<int> mergeSortedArray(vector<int> &A, vector<int> &B) 
 {
         // write your code here
         vector <int> data;
-        int val1,val2;
-        int len1,len2;
-        int i1=0,i2=0;
-        int temp;
-        int i,length;
-        len1=A.size();
-        len2=B.size();
-        length=len1+len2;
-        for(i=0;i<length;)
-        {
-            if(i1<len1&&i2<len2)
-            {
-                if(A[i1]>=B[i2])
-                {
-                    //data[i++]=B[i2++];
-                    //data.insert(i++,B[i2++]);
-                    data.push_back(B[i2++]);
-                    i++;
-                }
-                else
-                {
-                    //data[i++]=A[i1++];
-                    //data.insert(i++,A[i1++]);
-                    data.push_back(A[i1++]);
-                    i++;
-                }
-            }
-            else if(i1>=len1)
-            {
-                for(temp=i2;temp<len2;i2++)
-                {
-                    //data.insert(i++,B[i2]);
-                    //data[i++]=B[i2];
-                    data.push_back(B[i2]);
-                    i++;
-                }
-            }
-            else if(i2>=len2)
-            {
-                for(temp=i1;temp<len1;i1++)
-                {
-                    //data[i++]=A[i1];
-                    //data.insert(i++,A[i1]);
-                    data.push_back(A[i1]);
-                    i++;
-                }
-            }
-            else
-            {
-                break;
-            }
-        }
-        return data;
+	int len1, len2;
+	int i1 = 0, i2 = 0;
+	int temp;
+	int i, length;
+	len1 = A.size();
+	len2 = B.size();
+	length = len1 + len2;
+	for (i = 0; i<length;)
+	{
+		if (i1<len1&&i2<len2)
+		{
+			if (A[i1] >= B[i2])
+			{
+				data.push_back(B[i2++]);
+				i++;
+			}
+			else
+			{
+				data.push_back(A[i1++]);
+				i++;
+			}
+		}
+		else if (i1 >= len1)
+		{
+			for (temp = i2; temp<len2; temp++)
+			{
+				data.push_back(B[i2++]);
+				i++;
+			}
+		}
+		else if (i2 >= len2)
+		{
+			for (temp = i1; temp<len1; temp++)
+			{
+				data.push_back(A[i1++]);
+				i++;
+			}
+		}
+		else
+		{
+			break;
+		}
+	}
+	return data;
     }
